56e54c8e3100b314fc418dabd50a0581c47a846c
Evgeny Bobkin <evgen.ibqn@gmail.com>
2020-05-22 00:23:06 +0200

    add data sheet example

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,66 @@
-import React from "react"
+import React, { useState } from "react"
 
-const App = () => <div>test</div>
+import DataSheet from "react-datasheet"
+import "react-datasheet/lib/react-datasheet.css"
+import "./app.css"
+
+const App = () => {
+  const [state, setState] = useState({
+    grid: [
+      [
+        { readOnly: true, value: "/" },
+        { value: "A", readOnly: true },
+        { value: "B", readOnly: true },
+        { value: "C", readOnly: true },
+        { value: "D", readOnly: true },
+      ],
+      [
+        { readOnly: true, value: 1 },
+        { value: 1 },
+        { value: 3 },
+        { value: 3 },
+        { value: 3 },
+      ],
+      [
+        { readOnly: true, value: 2 },
+        { value: 2 },
+        { value: 4 },
+        { value: 4 },
+        { value: 4 },
+      ],
+      [
+        { readOnly: true, value: 3 },
+        { value: 1 },
+        { value: 3 },
+        { value: 3 },
+        { value: 3 },
+      ],
+      [
+        { readOnly: true, value: 4 },
+        { value: 2 },
+        { value: 4 },
+        { value: 4 },
+        { value: 4 },
+      ],
+    ],
+  })
+
+  return (
+    <DataSheet
+      data={state.grid}
+      valueRenderer={(cell) => cell.value}
+      onContextMenu={(e, cell, i, j) =>
+        cell.readOnly ? e.preventDefault() : null
+      }
+      onCellsChanged={(changes) => {
+        const grid = state.grid.map((row) => [...row])
+        changes.forEach(({ cell, row, col, value }) => {
+          grid[row][col] = { ...grid[row][col], value }
+        })
+        setState({ grid })
+      }}
+    />
+  )
+}
 
 export default App
